2b0af8e5829a9cc6bef233d3fe467cc07864c1ff
Muhdan Fyan Syah Sofian <muhdanfyan@gmail.com>
2021-10-28 23:26:51 +0800

    finishing

---
+++ main.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	"crudgo-mysql/controllers"
+	"crudgo-mysql/database"
+	"crudgo-mysql/entity"
+	"log"
+	"net/http"
+
+	"github.com/gorilla/mux"
+	_ "github.com/jinzhu/gorm/dialects/mysql" //Required for MySQL dialect
+)
+
+func main() {
+	initDB()
+	log.Println("Starting the HTTP server on port 8090")
+
+	router := mux.NewRouter().StrictSlash(true)
+	initaliseHandlers(router)
+	log.Fatal(http.ListenAndServe(":8090", router))
+}
+
+func initaliseHandlers(router *mux.Router) {
+	router.HandleFunc("/create", controllers.CreatePerson).Methods("POST")
+	router.HandleFunc("/get", controllers.GetAllPerson).Methods("GET")
+	router.HandleFunc("/get/{id}", controllers.GetPersonByID).Methods("GET")
+	router.HandleFunc("/update/{id}", controllers.UpdatePersonByID).Methods("PUT")
+	router.HandleFunc("/delete/{id}", controllers.DeletPersonByID).Methods("DELETE")
+}
+
+func initDB() {
+	config :=
+		database.Config{
+			ServerName: "localhost:3306",
+			User:       "root",
+			Password:   "",
+			DB:         "learning_demo",
+		}
+
+	connectionString := database.GetConnectionString(config)
+	err := database.Connect(connectionString)
+	if err != nil {
+		panic(err.Error())
+	}
+	database.Migrate(&entity.Person{})
+}
